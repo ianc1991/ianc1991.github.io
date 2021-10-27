10f6e5258b5fd56c811b6e492a82fe8b21d996cc
Ian <ianmchristian@gmail.com>
2021-10-26 20:01:10 -0400

    I think active property may be working

--- src/app/Components/navigation-bar/navigation-bar.component.ts
+++ src/app/Components/navigation-bar/navigation-bar.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navigation-bar',
   templateUrl: './navigation-bar.component.html',
   styleUrls: ['./navigation-bar.component.scss']
 })
-export class NavigationBarComponent implements OnInit {
+export class NavigationBarComponent implements OnInit, OnDestroy {
+  isActiveAboutMe: boolean = false;
+  isActivePortfolio: boolean = false;
+  isActiveContactMe: boolean = false;
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  ngOnDestroy(): void {
+    throw new Error('Method not implemented.');
+  }
 
   ngOnInit(): void {
-    
+    if (this.router.url === '/aboutme' || this.router.url === '/') {
+      this.isActiveAboutMe = true;
+      
+    }
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { PrimeNGConfig } from 'primeng/api';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +7,9 @@ import { PrimeNGConfig } from 'primeng/api';
 })
 export class AppComponent implements OnInit {
 
-  constructor(private primengConfig: PrimeNGConfig) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.primengConfig.ripple = true;
   }
 
 }
